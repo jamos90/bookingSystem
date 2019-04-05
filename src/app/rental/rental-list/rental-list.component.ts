19af58ef81f464fd2f61ee66df1b2920ed24ea25
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-05 15:37:25 -0400

    rental list components created and funcitoning

---
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bwm-rental-list',
+  templateUrl: './rental-list.component.html',
+  styleUrls: ['./rental-list.component.scss']
+})
+export class RentalListComponent implements OnInit {
+
+  rentals: any [] = [
+    {
+      id: 1,
+      title: 'Central Apartnemnt',
+      city: 'Tornoto',
+      street: 'Howard Park Ave',
+      category: 'apartment',
+      image: 'http//via/placeholder.com/350*250',
+      bedrooms: 3,
+      description: 'Very nice apartment',
+      datilyRate: 34,
+      shared: false,
+      createdAt: "05/04/19"
+    },
+    {
+      id: 2,
+      title: "Central Apartment 2",
+      city: "San Francisco",
+      street: "Main street",
+      category: "condo",
+      image: "http://via.placeholder.com/350x250",
+      bedrooms: 2,
+      description: "Very nice apartment",
+      dailyRate: 12,
+      shared: true,
+      createdAt: "24/12/2017"
+    },
+    {
+      id: 3,
+      title: "Central Apartment 3",
+      city: "Bratislava",
+      street: "Hlavna",
+      category: "condo",
+      image: "http://via.placeholder.com/350x250",
+      bedrooms: 2,
+      description: "Very nice apartment",
+      dailyRate: 334,
+      shared: true,
+      createdAt: "24/12/2017"
+    },
+    {
+      id: 4,
+      title: "Central Apartment 4",
+      city: "Berlin",
+      street: "Haupt strasse",
+      category: "house",
+      image: "http://via.placeholder.com/350x250",
+      bedrooms: 9,
+      description: "Very nice apartment",
+      dailyRate: 33,
+      shared: true,
+      createdAt: "24/12/2017"
+  }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
