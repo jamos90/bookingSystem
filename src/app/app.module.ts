fc5638d76cb6eb136f6324f064af1ea331ebffdc
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-05 15:36:05 -0400

    current files updated, rental module created and app module updated to reflect this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { TempComponent } from './temp/temp.component';
+
+import { RentalModule } from './rental/rental.module'
+
+const routes: Routes = [
+  {path: '', component:RentalComponent},
+  {path: 'temp', component: TempComponent}
+] 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TempComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
