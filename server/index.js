01ce6f0dec23ff3666efff61bdd35b9e038c3bfb
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-15 18:37:45 -0400

    index updated. asyn function added to clear db"

--- server/index.js
+++ server/index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/dev');
 const rental = require('./models/rental');
+const FakeDb = require('./fake-db');
+const rentalRoutes = require('./routes/rentals');
 
 // mongodb+srv://jamosamos90:M3lb0urne15!@cluster0-rxeiw.mongodb.net/test?retryWrites=true
 
-mongoose.connect(config.dbUri)
+mongoose.connect(config.dbUri).then(()=>{
+    const fakeDb = new FakeDb();
+    fakeDb.seedDb();
+})
+.catch((error)=>{
+    console.error(error);
+})
 
 const app = express();
 
-app.get('/rentals', function(req,res){
-    res.json({sucess: true})
-})
+app.use('/api/v1/rentals', rentalRoutes );
 
 const PORT = process.env.PORT || 3001
 
