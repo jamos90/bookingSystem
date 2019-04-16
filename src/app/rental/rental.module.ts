c905ac37feadf85b3c26719ea026be1cfa73741a
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-15 20:44:12 -0400

    rental module updated with http client

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,6 +1,7 @@
-import {NgModule} from '@angular/core';
-import{CommonModule} from '@angular/common';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import{ CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
@@ -29,7 +30,8 @@ const routes: Routes = [
     ],
     imports: [
         CommonModule,
-        RouterModule.forChild(routes)
+        RouterModule.forChild(routes),
+        HttpClientModule
     ],
     providers: [
         RentalService
