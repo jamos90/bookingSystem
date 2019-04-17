d55bbee24c688582289edeeed2814222d202c746
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-17 10:40:22 -0400

    uppercase pipe created and added to rental module

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import{ CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { NgPipesModule } from 'ngx-pipes';
+
 
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
@@ -9,6 +11,7 @@ import { RentalComponent } from './rental.component';
 
 import { RentalService } from './shared/rental.service';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+import { UppercasePipe } from '../common/pipes/uppercase.pipe';
 
 const routes: Routes = [
     {path: 'rentals', 
@@ -26,12 +29,15 @@ const routes: Routes = [
         RentalListComponent,
         RentalListItemComponent,
         RentalComponent,
-        RentalDetailComponent
+        RentalDetailComponent,
+        UppercasePipe
     ],
     imports: [
         CommonModule,
         RouterModule.forChild(routes),
-        HttpClientModule
+        HttpClientModule,
+        NgPipesModule,
+
     ],
     providers: [
         RentalService
