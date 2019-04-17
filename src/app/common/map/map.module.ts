003fd7b3eaff4f8e64513f1dbe8abb24e95df106
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-17 18:45:31 -0400

    common module added to map module

--- src/app/common/map/map.module.ts
+++ src/app/common/map/map.module.ts
@@ -1,10 +1,11 @@
 
 import { NgModule } from '@angular/core';
-import {MapComponent } from './map.component';
+import { MapComponent } from './map.component';
 import { AgmCoreModule } from '@agm/core';
 import { MapService } from '../map/map.service';
 import { CamelizePipe } from 'ngx-pipes';
-import { ApiKey}  from '../../config/dev';
+import { ApiKey }  from '../../config/dev';
+import { CommonModule } from '@angular/common';
 
 const key = new ApiKey;
 
@@ -20,14 +21,14 @@ const key = new ApiKey;
     
     AgmCoreModule.forRoot({
         apiKey: key.key
-      })
+      }),
+      CommonModule
 
 
   ],
   providers: [
     MapService,
     CamelizePipe,
-    
 
   ],
  
