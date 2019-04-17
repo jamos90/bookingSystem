3d582d08888d3603ceeb09d2ee2f89df8a3877c8
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-17 17:39:40 -0400

    rental detial updated with map tags passing location data through

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent} from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
 
-import { RentalModule } from './rental/rental.module'
+import { RentalModule } from './rental/rental.module';
 
 const routes: Routes = [
   {path: '', redirectTo: '/rentals', pathMatch: 'full'},
@@ -18,6 +18,7 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     HeaderComponent,
+    
  
   ],
   imports: [
