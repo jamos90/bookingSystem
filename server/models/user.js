e51027e4e780799aa9bbdd37bef3ff9718d94b8a
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-25 17:16:00 -0400

    bycrpyt added to user model, encrypts passwords before saving to db

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -27,6 +28,22 @@ const userSchema = new Schema({
     rentals: [{
         type: Schema.Types.ObjectId, ref: 'Rental'
     }]
+})
+
+//callback funciton is called just before data is saved to db. Good place to encode sensitive information.
+userSchema.pre('save', function(next){
+    //this context holds our user when it is being saved. Gives access to password etc
+    const user = this;
+    
+    //salt is required for hashing a password. Increases the security with greater numbers of rounds, first parameter of getSalt(). Here it is 10.
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            // Store hash in your password DB.
+            user.password = hash;
+            //calls the functions that our next in the que. Saving user to database for example. Is required.
+            next();
+        });
+    })
 })
 
 module.exports = mongoose.model('User', userSchema);
