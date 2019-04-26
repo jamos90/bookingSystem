1395eb4b062c6a72f8d9010ad4870d406ef6261d
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-26 10:40:40 -0400

    mongoose helper file set up. normalise errors function created

---
+++ server/helpers/mongoose.js
@@ -0,0 +1,12 @@
+module.exports = {
+    normailiseErrors: function(errors) {
+        let normailiseErrors = [];
+        for(propery in errors ) {
+            if(errors.hasOwnProperty(propery)){
+                normailiseErrors.push({title: propery,detail: errors[propery].message})
+            }
+        }
+
+        return normailiseErrors;
+    }
+}
