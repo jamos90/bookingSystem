48693f1a8d06cdd52e5836b7410a4df38ac07176
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-04-30 12:23:47 -0400

    register.ts router navigation added on successful reg. error messages added to template

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bwm-register',
@@ -9,8 +10,10 @@ import { AuthService } from '../shared/auth.service';
 export class RegisterComponent implements OnInit {
 
   formData: any = {};
+  errors: any[] = [];
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
     
@@ -19,10 +22,14 @@ export class RegisterComponent implements OnInit {
   register() {
     this.auth.register(this.formData).subscribe(
       () => {
-        console.log('sucesss')
+        //on successful regitration uses router to navigate to the login page.
+        this.router.navigate(['/login', {registered: 'success'}])
       },
+      //handles errors from the server. Reasigns our errors array to the error response to be displayed 
+      //on the view.
       (errorResponse)=>{
-        console.log(errorResponse);
+        this.errors = errorResponse.error.errors;
+        
       }
     )
   }
