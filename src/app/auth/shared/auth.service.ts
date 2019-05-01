dc3cb63ce3e3c23e2a47ad208318a6a37adaf0e1
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-05-01 13:53:34 -0400

    save token added to auth service. returning to front end

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import 'rxjs/Rx';
 
 
 @Injectable()
@@ -12,6 +13,17 @@ export class AuthService {
    public register(userData: any): Observable<any> {
        console.log(userData);
     return this.http.post('/api/v1/users/register', userData);
+    }
+
+    public login(userData: any): Observable<any> {
+        return this.http.post('/api/v1/users/auth', userData).map(
+            //implicet retrun to return the token to the login function in front end.
+            (token: string)=> this.saveToken(token));
+    }
+
+    private saveToken(token: string):string {
+        localStorage.setItem('bwm_auth', token);
+        return token;
     }
 
 }
