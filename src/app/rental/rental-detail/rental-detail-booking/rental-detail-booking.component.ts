5fc73ed451563815ae67a61744628803f58dab64
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-05-01 20:48:22 -0400

    rental detail booking, date picker code added

--- src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
+++ src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
@@ -15,4 +15,28 @@ export class RentalDetailBookingComponent implements OnInit {
   ngOnInit() {
   }
 
+  public daterange: any = {};
+
+    // see original project for full list of options
+    // can also be setup using the config service to apply to multiple pickers
+    public options: any = {
+        locale: { format: 'YYYY-MM-DD' },
+        alwaysShowCalendars: false,
+        opens: 'left'
+    };
+
+    public selectedDate(value: any, datepicker?: any) {
+        // this is the date the iser selected
+        console.log(value);
+
+        // any object can be passed to the selected event and it will be passed back here
+        datepicker.start = value.start;
+        datepicker.end = value.end;
+
+        // or manupulat your own internal property
+        this.daterange.start = value.start;
+        this.daterange.end = value.end;
+        this.daterange.label = value.label;
+    }
 }
+
