07048e1ed79385b7f7cfcd440ea28941afd9fe6e
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-05-28 13:37:37 -0400

    create rental compoents created

---
+++ src/app/rental/rental-create/rental-create.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Rental } from '../shared/rental.model';
+import { Router } from '@angular/router';
+import { RentalService } from '../shared/rental.service'
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'bwm-rental-create',
+  templateUrl: './rental-create.component.html',
+  styleUrls: ['./rental-create.component.scss']
+})
+export class RentalCreateComponent implements OnInit {
+
+  newRental: Rental;
+  rentalCategories = Rental.CATEGORIES;
+  errors: any[] = []
+  
+
+  constructor( private rentalService: RentalService,
+                private router: Router) { }
+
+  ngOnInit() {
+    this.newRental = new Rental();
+    this.newRental.shared = false;
+    this.errors = [];
+  }
+
+  createRental() {
+    console.log(this.newRental);
+    this.rentalService.createRental(this.newRental).subscribe(
+      (rental:Rental)=>{
+        this.router.navigate([`/rentals/${rental._id}`])
+
+      },
+      (errorResponse:HttpErrorResponse)=>{
+        this.errors = errorResponse.error.errors
+        console.log(this.errors);
+      }
+    )
+  }
+
+  handleImageChange() {
+    this.newRental.image = 'src/assets/images/create-rental.jpg'
+  }
+
+}
