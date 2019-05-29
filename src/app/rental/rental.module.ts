4d6a7f1c6ca33b4d9b943632f73411316767cb3d
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-05-29 16:07:07 -0400

    auth guard taken away from rental id page

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
     children: [
         {path: '', component: RentalListComponent},
         {path: 'new', component: RentalCreateComponent, canActivate: [AuthGuard]},
-        {path: ':rentalId', component: RentalDetailComponent, canActivate: [AuthGuard]},
+        {path: ':rentalId', component: RentalDetailComponent},
         {path: ':city/homes', component: RentalSearchComponent}
     ]
     },
