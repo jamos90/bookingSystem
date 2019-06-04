a27796a2cabd9e112f73c689590bb6281d6ea12c
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-06-04 17:53:43 -0400

    client: manage-rental, rental data coming back from server, console log not displaying yet

--- src/app/manage/manage-rental/manage-rental.component.ts
+++ src/app/manage/manage-rental/manage-rental.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RentalService } from '../../rental/shared/rental.service';
+import { Rental } from '../../rental/shared/rental.model';
+
 
 @Component({
   selector: 'bwm-manage-rental',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-rental.component.scss']
 })
 export class ManageRentalComponent implements OnInit {
+  
+  rentals: Rental[] = [];
 
-  constructor() { }
+  constructor( private rentalService:RentalService) { }
 
   ngOnInit() {
+    const userRentals = this.rentalService.getUserRentals()
+    userRentals.subscribe(
+      (data: Rental[])=>{
+        console.log(data);
+        this.rentals = data;
+        console.log('this is the rentals', this.rentals);
+    }),
+    ()=>{
+
+    }    
   }
+
+
 
 }
