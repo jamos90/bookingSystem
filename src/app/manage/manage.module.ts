79454be8b9bf64df0ea9583d8db5d0fe01c848cb
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-06-04 17:05:37 -0400

    manage module added to app module. routes create in manage module and tested in browser

---
+++ src/app/manage/manage.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ManageRentalComponent } from './manage-rental/manage-rental.component';
+import { ManageBookingComponent } from './manage-booking/manage-booking.component';
+import { ManageComponent } from './manage.component';
+
+
+const routes: Routes = [
+    { path: 'manage',
+    component: ManageComponent,
+    children: [
+        {
+        path:'rentals',
+        component: ManageRentalComponent
+        },
+        {
+        path:'bookings',
+        component: ManageBookingComponent
+        }
+    ]
+    },
+]
+
+@NgModule({
+    declarations:[
+        ManageRentalComponent,
+        ManageBookingComponent
+
+    ],
+    imports: [
+        RouterModule.forChild(routes)
+
+    ],
+})
+
+export class ManageModule{}
