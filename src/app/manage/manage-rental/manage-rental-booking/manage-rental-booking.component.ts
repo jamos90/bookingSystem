45f31444bb402da6e1556632aa20ef54c1ae0525
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-06-05 12:10:51 -0400

    client: manage rental bookings component created and updated with dynamic conente

---
+++ src/app/manage/manage-rental/manage-rental-booking/manage-rental-booking.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
+
+@Component({
+  selector: 'bwm-manage-rental-booking',
+  templateUrl: './manage-rental-booking.component.html',
+  styleUrls: ['./manage-rental-booking.component.scss']
+})
+
+export class ManageRentalBookingComponent implements OnInit {
+
+  @Input() rentalBookings: any;
+
+  bookings = this.rentalBookings;
+
+  constructor(public modalService: NgbModal) { }
+
+  ngOnInit() {
+    console.log(this.rentalBookings)
+  }
+
+  
+
+}
