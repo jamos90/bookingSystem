1f3c6c4f72637a8035d85d8b0b0959ad2a750742
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-06-11 09:44:51 -0400

    client: manage rental component update

--- src/app/manage/manage-rental/manage-rental.component.ts
+++ src/app/manage/manage-rental/manage-rental.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { RentalService } from '../../rental/shared/rental.service';
 import { Rental } from '../../rental/shared/rental.model';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { HttpErrorResponse } from '@angular/common/http';
+
+
 
 
 @Component({
@@ -11,20 +15,42 @@ import { Rental } from '../../rental/shared/rental.model';
 export class ManageRentalComponent implements OnInit {
   
   rentals: Rental[];
+  rentalDeleteIndex: number;
 
-  constructor( private rentalService:RentalService) { }
+  constructor( 
+    private rentalService:RentalService, 
+    private toastr: ToastsManager,
+    private vcr: ViewContainerRef
+    ) 
+    {
+      this.toastr.setRootViewContainerRef(vcr);
+    } 
 
   ngOnInit() {
     const userRentals = this.rentalService.getUserRentals()
     userRentals.subscribe(
       (data: Rental[])=>{
-        console.log(data);
+     
         this.rentals = data;
-        console.log('this is the rentals', this.rentals);
     }),
-    (error)=>{
-      console.error(`something has gone wrong ${error}`);
-
+    (errorResponse:HttpErrorResponse)=>{
+      this.toastr.error(errorResponse.error.errors[0].detail, 'Failed!');
     }    
   }
+
+  deleteRental(rentalId:string) {
+  
+    const deleteRental = this.rentalService.deleteRental(rentalId)
+
+    deleteRental.subscribe(()=>{
+      this.rentals.splice(this.rentalDeleteIndex,1);
+      this.rentalDeleteIndex = undefined;
+    }),
+    (errorResponse:HttpErrorResponse)=>{      
+      this.toastr.error(errorResponse.error.errors[0].detail, 'Failed!');
+      
+    }
+  }
+
+
 }
