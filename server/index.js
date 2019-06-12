cf3e10416c9f89a9b0aca41b4652ed5d1f4b9ba8
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-06-12 17:41:26 -0400

    pre deploy set up

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require('./config/dev');
+const config = require('./config/index');
 const rental = require('./models/rental');
 const FakeDb = require('./fake-db');
+const path = require('path');
 
 const rentalRoutes = require('./routes/rentals');
 const userRoutes = require('./routes/user');
@@ -12,8 +13,10 @@ const bookingRoutes = require('./routes/booking')
 // mongodb+srv://jamosamos90:M3lb0urne15!@cluster0-rxeiw.mongodb.net/test?retryWrites=true
 
 mongoose.connect(config.dbUri).then(()=>{
-    const fakeDb = new FakeDb();
-    // fakeDb.seedDb();
+    if(process.env.NODE_ENV !== 'production') {
+        const fakeDb = new FakeDb();
+        // fakeDb.seedDb();
+    }
 })
 .catch((error)=>{
     console.error(error);
@@ -27,8 +30,18 @@ app.use('/api/v1/rentals', rentalRoutes );
 app.use('/api/v1/users', userRoutes );
 app.use('/api/v1/bookings', bookingRoutes);
 
+if(process.env.NODE_ENV === 'production') {
+    const appPath = path.join(__dirname, '..' , 'dist');
+    app.use(express.static(appPath));
+    app.get('*', function(req,res){
+    res.sendFile(path.resolve(appPath, 'index.html'));
+})
+}
+
+
+
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, function(){
-    console.log("im running");
+   
 })
