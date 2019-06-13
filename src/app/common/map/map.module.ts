8798ef230d2b760206660711e2e639afbaa82419
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-06-13 15:36:19 -0400

    map module updated

--- src/app/common/map/map.module.ts
+++ src/app/common/map/map.module.ts
@@ -4,11 +4,9 @@ import { MapComponent } from './map.component';
 import { AgmCoreModule } from '@agm/core';
 import { MapService } from '../map/map.service';
 import { CamelizePipe } from 'ngx-pipes';
-import { ApiKey }  from '../../config/dev';
 import { CommonModule } from '@angular/common';
 
-const key = new ApiKey;
-const apiKey = key.key;
+
 
 @NgModule({
   declarations: [
