eb70e775cc44e230d6ffd28c531e17008566f090
Jessica Ruiters <Jessica@Jessicas-MacBook-Pro.local>
2019-06-20 17:56:06 -0400

    index.js changes

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,5 @@
-if(process.env.NODE_ENV) {
-    module.exports = require('./prod.js');
-}
-else {
-    module.exports = require('./dev.js');
-}
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+  } else {
+    module.exports = require('./dev');
+  }
